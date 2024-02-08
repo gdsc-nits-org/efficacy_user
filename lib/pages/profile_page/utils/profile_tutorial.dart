4a33dbcf328e1d60ae722c8486af256eb48181c8
git-ADG <adg8503@gmail.com>
2024-02-08 21:04:25 +0530

    guide added to profile page

---
+++ lib/pages/profile_page/utils/profile_tutorial.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
+
+import '../../../widgets/coach_mark_desc/coach_mark_desc.dart';
+
+void showProfileTutorial(BuildContext context, GlobalKey editProfileKey,
+    GlobalKey deleteProfileKey) {
+  List<TargetFocus> targets = getTargets(
+    editProfileKey,
+    deleteProfileKey,
+  );
+
+  TutorialCoachMark(
+    hideSkip: true,
+    useSafeArea: true,
+    targets: targets, // List<TargetFocus>
+  ).show(context: context);
+}
+
+List<TargetFocus> getTargets(
+  GlobalKey editProfileKey,
+  GlobalKey deleteProfileKey,
+) {
+  return [
+    TargetFocus(
+      identify: "Edit Profile",
+      keyTarget: editProfileKey,
+      contents: [
+        TargetContent(
+          align: ContentAlign.bottom,
+          builder: (context, controller) {
+            return CoachmarkDesc(
+              heading: "Edit Profile",
+              text: "Click here to edit profile.",
+              onNext: () {
+                controller.next();
+              },
+              onSkip: () {
+                controller.skip();
+              },
+            );
+          },
+        )
+      ],
+    ),
+    TargetFocus(
+      identify: "Delete Profile",
+      keyTarget: deleteProfileKey,
+      contents: [
+        TargetContent(
+          align: ContentAlign.top,
+          builder: (context, controller) {
+            return CoachmarkDesc(
+              heading: "Edit Club Position",
+              text: "Click here to delete your profile.",
+              onNext: () {
+                controller.next();
+              },
+              onSkip: () {
+                controller.skip();
+              },
+            );
+          },
+        )
+      ],
+    ),
+  ];
+}
