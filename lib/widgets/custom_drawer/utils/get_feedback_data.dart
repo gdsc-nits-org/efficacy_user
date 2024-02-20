8d7e0efd60fbb038916585185568549860ddc6ec
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2024-02-21 03:28:11 +0530

    Reverting changes to feedback due to overflow

--- lib/widgets/custom_drawer/utils/get_feedback_data.dart
+++ lib/widgets/custom_drawer/utils/get_feedback_data.dart
@@ -29,8 +29,6 @@ Future<Uint8List> getFeedBackData(UserFeedback feedback) async {
               pw.Column(
                 crossAxisAlignment: pw.CrossAxisAlignment.start,
                 children: [
-                  pw.Text("Efficacy User Report:", style: pw.TextStyle(font: ttf)),
-                  pw.SizedBox(height: 10),
                   pw.Text("Device Info", style: pw.TextStyle(font: ttf)),
                   pw.Text(
                     "1. App: User",
@@ -60,14 +58,14 @@ Future<Uint8List> getFeedBackData(UserFeedback feedback) async {
                     "7. Hardware: ${androidInfo.hardware}",
                     style: pw.TextStyle(font: ttf),
                   ),
-                  pw.SizedBox(height: 20),
-                  pw.Text("Issue:", style: pw.TextStyle(font: ttf)),
-                  pw.SizedBox(height: 10),
-                  pw.Text(feedback.text, style: pw.TextStyle(font: ttf)),
                 ],
               ),
             ],
           ),
+          pw.SizedBox(height: 20),
+          pw.Text("Issue:", style: pw.TextStyle(font: ttf)),
+          pw.SizedBox(height: 10),
+          pw.Text(feedback.text, style: pw.TextStyle(font: ttf)),
         ];
       },
     ),
