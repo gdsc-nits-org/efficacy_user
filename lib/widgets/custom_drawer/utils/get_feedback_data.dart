ff350e3e89886c04f779fc9b86a7b9c10b7f0ac3
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2024-02-21 00:24:01 +0530

    Changing feedback to easily identify

--- lib/widgets/custom_drawer/utils/get_feedback_data.dart
+++ lib/widgets/custom_drawer/utils/get_feedback_data.dart
@@ -29,6 +29,8 @@ Future<Uint8List> getFeedBackData(UserFeedback feedback) async {
               pw.Column(
                 crossAxisAlignment: pw.CrossAxisAlignment.start,
                 children: [
+                  pw.Text("Efficacy User Report:", style: pw.TextStyle(font: ttf)),
+                  pw.SizedBox(height: 10),
                   pw.Text("Device Info", style: pw.TextStyle(font: ttf)),
                   pw.Text(
                     "1. App: User",
@@ -58,14 +60,14 @@ Future<Uint8List> getFeedBackData(UserFeedback feedback) async {
                     "7. Hardware: ${androidInfo.hardware}",
                     style: pw.TextStyle(font: ttf),
                   ),
+                  pw.SizedBox(height: 20),
+                  pw.Text("Issue:", style: pw.TextStyle(font: ttf)),
+                  pw.SizedBox(height: 10),
+                  pw.Text(feedback.text, style: pw.TextStyle(font: ttf)),
                 ],
               ),
             ],
           ),
-          pw.SizedBox(height: 20),
-          pw.Text("Issue:", style: pw.TextStyle(font: ttf)),
-          pw.SizedBox(height: 10),
-          pw.Text(feedback.text, style: pw.TextStyle(font: ttf)),
         ];
       },
     ),
